91e8ac50007d0c92b660f50271a820d4c05fbf5c
SemC0ndicao <jokerplaybr12@gmail.com>
2025-10-16 20:15:41 -0300

    renamed:    modelos frontend/tema escuto.png -> modelos frontend/tema escuro.png 	modified:   spider0/spider0/spiders/metropoles.py

--- spider0/spider0/spiders/metropoles.py
+++ spider0/spider0/spiders/metropoles.py
@@ -9,3 +9,9 @@ class MetropolesSpider(scrapy.Spider):
     def parse(self, response):
         for href in response.css('article h2 a::attr(href)').extract():
             yield response.follow(href, self.parse_article)
+        
+        titulo = response.css('h1::text').get()
+        yield {
+        'titulo': titulo
+    }
+    
